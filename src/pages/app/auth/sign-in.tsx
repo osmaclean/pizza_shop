71b1564bacb25e914ac1ef34a82893147887eccd
Maclean <contatolucasmaclean@gmail.com>
2024-09-04 11:30:07 -0300

    feat: Implementation of react-query in the application, validation of environment variables, creation of getProfile, getManagedRestaurant, registerRestaurant and signIn services, implementation of skeleton to improve user experience, creation of modal for editing the profile

--- src/pages/app/auth/sign-in.tsx
+++ src/pages/app/auth/sign-in.tsx
@@ -1,9 +1,11 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
 import * as z from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -15,17 +17,25 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>
 
 export default function SignIn() {
+  const [searchParams] = useSearchParams()
+
   const {
     handleSubmit,
     register,
     formState: { isSubmitting },
-  } = useForm<SignInForm>()
+  } = useForm<SignInForm>({
+    defaultValues: { email: searchParams.get('email') ?? '' },
+  })
+
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
 
   const handleSignIn = async (data: SignInForm) => {
     try {
       console.log(data)
 
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+      await authenticate({ email: data.email })
 
       toast.success('Enviamos um link de autenticação para o seu e-mail.', {
         action: {
